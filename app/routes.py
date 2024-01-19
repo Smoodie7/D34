c879d38bded83ed33c50fad9a10044573ef9aded
Smoodie <rayandaoudi08@gmail.com>
2024-01-19 06:19:17 +0100

    Dynamic website name
    Added dynamic changes to website name depending on the user.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("main", __name__)
 
+def get_website_info(website_name):
+    website_name_mapping = {
+        'sluttyleaks': "SluttyLeaks",
+        'nsfwvids': "NSFWVids"
+    }
+
+    formatted_name = website_name_mapping.get(website_name.lower(), website_name.capitalize())
+    return formatted_name
+
 @bp.route("/")
 def home():
-    return render_template("index.html")
+    default_website_name = "sluttyleaks"  # Default value in case the URL parameter is not provided
+    requested_website_name = request.args.get('website_name', default_website_name)
+
+    website_name = get_website_info(requested_website_name)
+    logo_path = f"assets/png/logo/{requested_website_name.lower()}-logo.png"
+
+    return render_template("index.html", website_name=website_name, logo_path=logo_path)
