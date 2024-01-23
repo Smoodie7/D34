87c6cec23e7dd8fa48e083c78c9ac16b84e03a7a
Smoodie <rayandaoudi08@gmail.com>
2024-01-23 07:01:17 +0100

    More stable and deployment ready version
    -Added robots.txt link for search engine crawlers (half broken)
    -/home route who leads to /
    -Procfile and requirements for deployment standards validation
    -template secret key
    -Better error page
    -General html file structure optimizations and improvements (unecessary scripts loaded at the end instead of the beginning)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, send_from_directory,request
 
-bp = Blueprint("main", __name__)
+bp = Blueprint("main", __name__, static_folder='app')
 
 def get_website_info(website_name):
     website_name_mapping = {
@@ -11,6 +11,7 @@ def get_website_info(website_name):
     formatted_name = website_name_mapping.get(website_name.lower(), website_name.capitalize())
     return formatted_name
 
+@bp.route("/home")
 @bp.route("/")
 def home():
     default_website_name = "sluttyleaks"  # Default value in case the URL parameter is not provided
@@ -20,3 +21,9 @@ def home():
     logo_path = f"assets/png/logo/{requested_website_name.lower()}-logo.png"
 
     return render_template("index.html", website_name=website_name, logo_path=logo_path)
+
+# Others
+@bp.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(bp.static_folder, 'robots.txt')
+
